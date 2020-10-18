9001ee728c29124dc52a6216b988c9c1cc018591
cdnexport <fudgemaster3000@hotmail.com>
2020-10-18 11:27:51 -0500

    Can now add sites

--- options/options.js
+++ options/options.js
@@ -1,6 +1,25 @@
 "use strict";
 
-function addRow(site, url) {
+function addMenu(site, url) {
+    const menuId = `${site}_0`;
+    chrome.storage.sync.get("sites", (response) => {
+        response.sites.push({
+            site: site,
+            url: url,
+            menuId: menuId
+        });
+        chrome.storage.sync.set({sites: response.sites});
+    });
+    chrome.contextMenus.create({
+        id: menuId,
+        parentId: "quickSearchSelection",
+        title: site,
+        type: "normal",
+        contexts: ["selection"]
+    });
+}
+
+function addRow(site, url, initLoad = false) {
     const siteTable = document.querySelector("#siteTable");
 
     let siteCell = document.createElement("td");
@@ -18,17 +37,36 @@ function addRow(site, url) {
     newRow.appendChild(urlCell);
     newRow.appendChild(btnCell);
     tableBody.appendChild(newRow);
+
+    if (!initLoad) {
+        addMenu(site, url);
+    }
+    document.querySelector("#site").value = "";
+    document.querySelector("#url").value = "";
 }
 
 function loadSites() {
     chrome.storage.sync.get("sites", (response) => {
-        console.log(response);
         for (let i = 0; i < response.sites.length; i++) {
             const element = response.sites[i];
-            addRow(element.site, element.url);
+            addRow(element.site, element.url, true);
         }
     });
 }
+
+function validateData() {//todo: implement
+    return {valid: true, message: "success"};
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     loadSites();
+    document.querySelector("#addButton").addEventListener("click", (e) => {
+        var validation = validateData();
+        if (validation.valid) {
+            addRow(
+                document.querySelector("#site").value,
+                document.querySelector("#url").value
+            );
+        }
+    });
 });
